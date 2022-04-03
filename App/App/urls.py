f1e65bcf6d264f3bb7e9677ccc6057093ef57b99
vikram123 <vik.kumar825@gmail.com>
2022-04-03 11:55:29 +0530

    Seo sitemap

--- App/App/urls.py
+++ App/App/urls.py
@@ -3,10 +3,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
+from Subapp01.sitemap import PostSitemap
+
+sitemaps = {
+    'posts':PostSitemap
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('Subapp01.urls'))
+    path('' , include('Subapp01.urls')),
+    path(
+        'sitemap.xml',sitemap, {'sitemaps':sitemaps}, name='django.contrib.sitemaps.views.sitemap'
+        )
 ]
 
 
--- App/Subapp01/models.py
+++ App/Subapp01/models.py
@@ -81,10 +81,11 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
-
     def get_absolute_url(self):
-        return reverse("add_post", args=(str(self.id)))
+        return reverse('articleView', args = [self.slug])
+#new
+    # def get_absolute_url(self):
+    #     return reverse("add_post", args=(str(self.id)))
     
 
 
